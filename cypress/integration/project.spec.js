f431eba696abad7a5fd380e90bc597e2833f6918
Mauricio Homma <mhhomma@gmail.com>
2020-04-06 17:33:31 -0300

    Add base for requisites 8 and 9

--- cypress/integration/project.spec.js
+++ cypress/integration/project.spec.js
@@ -1,8 +1,13 @@
+const API_URL = "https://api.mercadolibre.com/sites/MLB/search?q=$computador"
 const PROJECT_URL = './JulioCezar/index.html'
+
+const LOADING = '.loading';
 const ITEM_SELECTOR = '.item';
 const ADD_CART_BUTTON = '.item__add'
 const CART_ITEMS = '.cart__items'
 const EMPTY_CART_BUTTON = '.button-items'
+const TOTAL_PRICE = '.totalPrice_items'
+
 describe('Shopping Cart Project', () => {
   
   beforeEach(() => {
@@ -49,6 +54,40 @@ describe('Shopping Cart Project', () => {
       .children()
       .should('have.length', 0)
   });
-  it('Custo total do carrinho de compras');
-  it('Adicionar um texto de "loading" durante uma requisição à API');
+  it('Custo total do carrinho de compras', () => {
+    let totalValue = 0;
+    cy.request(API_URL)
+      .then((response) => {
+        totalValue += response.body.results[9].price;
+        console.log(totalValue);
+        totalValue += response.body.results[40].price;
+        console.log(totalValue);
+        totalValue += response.body.results[23].price;
+        console.log(totalValue);
+        
+      })
+    cy.visit(PROJECT_URL);
+    cy.get(ITEM_SELECTOR)
+      .should('exist')
+      .eq(9)
+      .children(ADD_CART_BUTTON)
+      .click();
+    cy.get(ITEM_SELECTOR)
+      .should('exist')
+      .eq(40)
+      .children(ADD_CART_BUTTON)
+      .click();
+    cy.get(ITEM_SELECTOR)
+      .should('exist')
+      .eq(23)
+      .children(ADD_CART_BUTTON)
+      .click();
+    cy.get(TOTAL_PRICE)
+      .should('have.value', totalValue)
+  });
+  it('Adicionar um texto de "loading" durante uma requisição à API', () => {
+    cy.request(PROJECT_URL)
+    cy.get(LOADING)
+      .should('exist')
+  });
 });
