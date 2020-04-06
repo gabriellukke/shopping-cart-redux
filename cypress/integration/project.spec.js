c9be82a8f3ab155de430e0016404a629689714c1
Jean Vasconcelos <jean.vasconcelos@betrybe.com>
2020-04-06 15:54:10 -0300

    add test stub

--- cypress/integration/project.spec.js
+++ cypress/integration/project.spec.js
@@ -1,3 +1,11 @@
 describe('Shopping Cart Project', () => {
-
+  it('Listagem de produtos');
+  it('Adicione o produto ao carrinho de compras');
+  it('Remova o item do carrinho de compras ao clicar nele');
+  it('Salve o carrinho de compras no **LocalStorage**');
+  it('Carregue o carrinho de compras através do **LocalStorage** ao iniciar a página');
+  it('Some o valor total dos itens do carrinho de compras de forma assíncrona');
+  it('Botão para limpar carrinho de compras');
+  it('Custo total do carrinho de compras');
+  it('Adicionar um texto de "loading" durante uma requisição à API');
 });
