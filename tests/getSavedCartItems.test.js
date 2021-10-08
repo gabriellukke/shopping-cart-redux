05980286265f767b78a5589c9f50528deb6283db
Marco Galindo <marco1408@gmail.com>
2021-10-08 10:15:03 -0300

    adicionando mocks para local storage

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -1,5 +1,11 @@
 const getSavedCartItems = require('../helpers/getSavedCartItems');
 
+Object.defineProperty(window, 'localStorage', {
+  value: {
+    getItem: jest.fn(),
+  },
+});
+
 describe('A função getSavedCartItems', () => {
   // implemente seus testes aqui
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -1,5 +1,11 @@
 const saveCartItems = require('../helpers/saveCartItems');
 
+Object.defineProperty(window, 'localStorage', {
+  value: {
+    setItem: jest.fn(),
+  },
+});
+
 describe('A função saveCartItems', () => {
   // implemente seus testes aqui
 });
