56f1b946dabe3dfbc2e5ae769767651c633abaff
Matheus Alexandre <alexandremhm@gmail.com>
2021-10-08 10:29:57 -0300

    Add pasta test

---
+++ tests/fetchProducts.test.js
@@ -0,0 +1,8 @@
+const fetchSimulator = require('../mocks/fetchSimulator');
+const { fetchProducts } = require('../helpers/fetchProducts');
+
+describe('1 - Teste a função fecthProducts', () => {
+  
+  fail('Teste vazio');
+
+});
