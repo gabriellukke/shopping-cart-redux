85da019d33ef3360345b35867ec14c913dfb167d
Matheus Alexandre <alexandremhm@gmail.com>
2021-10-13 08:16:27 -0300

    Update tests/fetchProducts.test.js
    Co-authored-by: Juliana Happatsch <jhappatsch@gmail.com>

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,7 +1,7 @@
 const fetchSimulator = require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 
-window.fetch = jest.fn(fetchSimulator)
+window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
 
