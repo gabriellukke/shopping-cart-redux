b1654df0fd13022da278b8ce03d7bce3a0fa3d0e
gabriellukke <gabriellukke@outlook.com>
2022-05-18 20:32:12 -0300

    Feat: Requirement 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,31 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('fetchProducts is a function', () => {
+    expect(fetchProducts).toBeInstanceOf(Function);
+  });
+
+  it('Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', () => {
+    fetchProducts('computador');
+
+    expect(global.fetch).toHaveBeenCalled();
+  });
+
+  it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', () => {
+    fetchProducts('computador');
+
+    expect(global.fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+
+  it('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
+    const mockValue = await fetchProducts('computador');
+
+    expect(mockValue).toStrictEqual(computadorSearch);
+  });
+
+  it('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: "You must provide an url".', () => {
+    const mockError = await fetchProducts();
+
+    expect(mockError).toThrowError('You must provide an url');
+  });
 });
