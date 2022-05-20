854628bbeaa865508ba778fd6d1c99b35712ef36
gabriellukke <gabriellukke@outlook.com>
2022-05-20 11:40:01 -0300

    Requirement 1 - Test descriptions adjustments

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,29 +3,29 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  it('fetchProducts is a function', () => {
+  it('Teste se fetchProducts é uma função', () => {
     expect(fetchProducts).toBeInstanceOf(Function);
   });
 
-  it('Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', () => {
+  it('Função fetch é chamada ao executar a função fetchProducts com o argumento "computador"', () => {
     fetchProducts('computador');
 
     expect(global.fetch).toHaveBeenCalled();
   });
 
-  it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', () => {
+  it('Ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', () => {
     fetchProducts('computador');
 
     expect(global.fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   });
 
-  it('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
+  it('O retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
     const mockValue = await fetchProducts('computador');
 
     expect(mockValue).toStrictEqual(computadorSearch.results);
   });
 
-  it('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: "You must provide an url".', async () => {
+  it('Ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: "You must provide an url".', async () => {
     const mockError = await fetchProducts();
     console.log(mockError);
 
